78df6932373ca6b8f3158e28d7b0f0580d83435c
Anıl Aydın <anil.aydin@enuygun.com>
2021-09-01 07:13:36 +0300

    pics project final commit

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return (
-      <img
-        key={image.id}
-        src={image.urls.regular}
-        alt={image.alt_description}
-      />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
 
   return <div className="image-list">{images}</div>;
